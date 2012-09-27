f18b2d64ec72c254c957d29e6862b2934d374502
Anton Storozhev <vinni-au@users.noreply.github.com>
2012-09-27 19:16:50 +0000

    8

--- unix/hw3/task8.sh
+++ unix/hw3/task8.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 #Bubble sort
 
-function exch {
-	t=$0
-	$0=$1
-	$1=$t
-}
-
-function compexch {
-	if [$0 -le $1]
-		exch $0 $1
-	fi
-}
+length=0
+for arg in $*
+do
+	array[$length]=$arg
+    length=`expr $length + 1`
+done
 
 
+for ((i=0; i < $length; i++))
+do
+	for ((j=`expr $length - 1`; j > $i; j--))
+	do
+		if [ ${array[`expr $j - 1`]} -gt ${array[$j]} ]
+		then
+			t=${array[`expr $j - 1`]}
+			array[`expr $j - 1`]=${array[$j]}
+			array[$j]=$t
+		fi
+	done
+done
+
+for ((i=0; i < $length; i++))
+do
+	echo ${array[$i]}
+done
