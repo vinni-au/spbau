1d30249a5462d3c39cf32b0509670ac7a876c083
Anton Storozhev <vinni-au@users.noreply.github.com>
2012-10-01 20:28:02 +0000

    Spiral

--- unix/pr4.sh
+++ unix/pr4.sh
@@ -4,17 +4,81 @@ function puts #x, y, string
 {
 	tput sc
 	tput cup $1 $2
-	echo "$3"
+	echo -ne "\033[47m \033[0m"
 	tput rc
 }
 
 tput init
 a=0;
-for i in $(seq 0 23)
+
+left=0
+right=`tput cols`
+right=`expr $right - 1`
+top=0
+bottom=`tput lines`
+bottom=`expr $bottom - 2`
+
+for i in $(seq $left $right)
 do
-	for j in $(seq 0 79)
-	do
-		puts $i $j "*"
-	done
+	puts $top $i "*"
 done
 
+for i in $(seq $top $bottom)
+do
+	puts $i $right "*"
+done
+
+for i in $(seq $right -1 $left)
+do
+	puts $bottom $i "*"
+done
+
+top=`expr $top + 2`
+
+while ( true )
+do
+
+	if [ $bottom -le `expr $top + 1` ]; then
+		exit
+	fi
+
+	for i in $(seq $bottom -1 $top)
+	do
+		puts $i $left "*"
+	done
+
+	right=`expr $right - 2`
+
+	if [ $left -ge `expr $right - 1` ]; then
+		exit
+	fi
+
+	for i in $(seq $left $right)
+	do
+		puts $top $i "*"
+	done
+
+	bottom=`expr $bottom - 2`
+
+	if [ $top -ge `expr $bottom - 1` ]; then
+		exit
+	fi
+
+	for i in $(seq $top $bottom)
+	do
+		puts $i $right "*"
+	done
+
+	left=`expr $left + 2`
+
+	if [ $left -ge `expr $right - 1` ]; then
+		exit
+	fi
+	for i in $(seq $right -1 $left)
+	do
+		puts $bottom $i "*"
+	done
+
+	top=`expr $top + 2`
+
+done
--- unix/test/2.sh
+++
@@ -1,9 +0,0 @@
-for ((i=0; i < 8; i++))
-do
-	x=`expr $i % 2`
-	if [ $x -eq 0 ]
-	then
-		echo -ne "  "
-	fi
-	echo -e "\033[47m  \033[0m  \033[47m  \033[0m  \033[47m  \033[0m  \033[47m  \033[0m"
-done
